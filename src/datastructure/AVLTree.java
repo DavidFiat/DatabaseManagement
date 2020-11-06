3e387e18099da94ba410dc1c0aefed2850452209
David <davidfiat24@gmail.com>
2020-11-06 02:06:02 -0500

    AVL Finished

--- src/datastructure/AVLTree.java
+++ src/datastructure/AVLTree.java
@@ -43,59 +43,81 @@ public class AVLTree<K extends Comparable<K>, V> extends BinarySearchTree<K, V> 
 	private AVLNode<K, V> rotate(AVLNode<K, V> b) {
 		if (b.getBalanceFactor() == 2) {
 			if (((AVLNode<K, V>) b.getLeft()).getBalanceFactor() >= 0) {
-				return leftLeftCase(b);
+				leftLeftCase(b);
 
 			} else {
-				return leftRightCase(b);
+				leftRightCase(b);
 			}
 
 		} else if (b.getBalanceFactor() == -2) {
 
 			if (((AVLNode<K, V>) b.getRight()).getBalanceFactor() <= 0) {
-				return rightRightCase(b);
+				rightRightCase(b);
 
 			} else {
-				return rightLeftCase(b);
+				rightLeftCase(b);
 			}
 		}
 
 		return b;
 	}
 
-	private AVLNode<K, V> leftLeftCase(AVLNode<K, V> b) {
-		return rightRotation(b);
+	private void leftLeftCase(AVLNode<K, V> b) {
+		rightRotation(b);
 	}
 
-	private AVLNode<K, V> leftRightCase(AVLNode<K, V> b) {
-		b.setLeft(leftRotation((AVLNode<K, V>) b.getLeft()));
-		return leftLeftCase(b);
+	private void leftRightCase(AVLNode<K, V> b) {
+		leftRotation((AVLNode<K, V>) b.getLeft());
+		leftLeftCase(b);
 	}
 
-	private AVLNode<K, V> rightRightCase(AVLNode<K, V> b) {
-		return leftRotation(b);
+	private void rightRightCase(AVLNode<K, V> b) {
+		leftRotation(b);
 	}
 
-	private AVLNode<K, V> rightLeftCase(AVLNode<K, V> b) {
-		b.setRight(rightRotation((AVLNode<K, V>) b.getRight()));
-		return rightRightCase(b);
+	private void rightLeftCase(AVLNode<K, V> b) {
+		rightRotation((AVLNode<K, V>) b.getRight());
+		rightRightCase(b);
 	}
 
-	private AVLNode<K, V> leftRotation(AVLNode<K, V> b) {
-		AVLNode<K, V> a = (AVLNode<K, V>) b.getRight();
-		b.setRight(a.getLeft());
-		a.setLeft(b);
-		update(b);
-		update(a);
-		return a;
+	private void leftRotation(AVLNode<K, V> b) {
+		AVLNode<K, V> parent = (AVLNode<K, V>) b.getParent();
+		AVLNode<K, V> right = (AVLNode<K, V>) b.getRight();
+		b.setRight(right.getLeft());
+		if (right.getLeft() != null) {
+			right.getLeft().setParent(b);
+		}
+		right.setLeft(b);
+		b.setParent(right);
+		right.setParent(parent);
+		if (parent != null) {
+			if (parent.getLeft().compareTo(b) == 0) {
+				parent.setLeft(right);
+			} else {
+				parent.setRight(right);
+			}
+		}
+
 	}
 
-	private AVLNode<K, V> rightRotation(AVLNode<K, V> b) {
-		AVLNode<K, V> a = (AVLNode<K, V>) b.getLeft();
-		b.setLeft(a.getRight());
-		a.setRight(b);
-		update(b);
-		update(a);
-		return a;
+	private void rightRotation(AVLNode<K, V> b) {
+		AVLNode<K, V> parent = (AVLNode<K, V>) b.getParent();
+		AVLNode<K, V> left = (AVLNode<K, V>) b.getLeft();
+		b.setLeft(left.getRight());
+		if (left.getRight() != null) {
+			left.getRight().setParent(b);
+		}
+		left.setRight(b);
+		b.setParent(left);
+		left.setParent(parent);
+		if (parent != null) {
+			if (parent.getLeft().compareTo(b) == 0) {
+				parent.setLeft(left);
+			} else {
+				parent.setRight(left);
+			}
+		}
+
 	}
 
 }
