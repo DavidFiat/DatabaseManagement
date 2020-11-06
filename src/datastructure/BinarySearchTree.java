0e8ea439164d649e17fdc9f4aa60f2cba5f6f850
David <davidfiat24@gmail.com>
2020-11-06 01:18:58 -0500

    BST Finished

--- src/datastructure/BinarySearchTree.java
+++ src/datastructure/BinarySearchTree.java
@@ -49,33 +49,42 @@ public class BinarySearchTree<K extends Comparable<K>, V> {
 	}
 
 	protected boolean delete(BinarySearchTreeNode<K, V> bstn) {
+		BinarySearchTreeNode<K, V> parent = bstn.getParent();
+		int i = 0;
+		if (parent.getLeft().compareTo(bstn) == 0) {
+			i = 1;
+		} else {
+			i = 2;
+		}
 
 		if (bstn.getLeft() == null) {
-			bstn = bstn.getRight();
+			switch (i) {
+			case 1:
+				parent.setLeft(bstn.getRight());
+
+			case 2:
+				parent.setRight(bstn.getRight());
+			}
 			return true;
 		} else if (bstn.getRight() == null) {
-			bstn = bstn.getLeft();
+			switch (i) {
+			case 1:
+				parent.setLeft(bstn.getLeft());
+			case 2:
+				parent.setRight(bstn.getLeft());
+			}
 			return true;
 
 		} else {
-
 			if (bstn.getLeft().getHeight() > bstn.getRight().getHeight()) {
-
 				BinarySearchTreeNode<K, V> s = findMax(bstn.getLeft());
-				BinarySearchTreeNode<K, V> parent = bstn.getParent();
-				
-
-				bstn = s;
-
+				bstn.setKey(s.getKey());
+				bstn.setValue(s.getValue());
 				return delete(s);
-
 			} else {
-
-				// Swap the value of the successor into the node.
 				BinarySearchTreeNode<K, V> s = findMin(bstn.getRight());
-				BinarySearchTreeNode<K, V> r = bstn.getRight();
-				bstn = s;
-
+				bstn.setKey(s.getKey());
+				bstn.setValue(s.getValue());
 				return delete(s);
 			}
 		}
