8a15f38ad447056e94fbecbc9c72aa043182f4dc
Julian Andres Rivera Carrillo <julianandresrivera29@hotmail.com>
2020-11-07 00:54:38 -0500

    GUI finished

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,19 +1,32 @@
 package model;
 
 public class Person {
-
+	
+	public final static char MASCULINE = 'M';
+	public final static char FEMENINE = 'F';
+	
 	private String name;
+	
 	private String code;
+	
 	private String lastName;
+	
 	private char sex;
+	
 	private String date;
 	
-	public Person(String n, String c, String l, char s, String d) {
+	private int height;
+	
+	private String nationality;
+	
+	public Person(String c, String n, String l, char s, String d, int he, String nat) {
 		name = n;
 		code = c;
 		lastName = c;
 		sex = s;
 		date = d;
+		height = he;
+		nationality = nat;
 	}
 
 	public String getName() {
@@ -34,6 +47,42 @@ public class Person {
 
 	public String getDate() {
 		return date;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
 	}
 	
 	
--- src/ui/PrincipalController.java
+++ src/ui/PrincipalController.java
@@ -1,24 +1,42 @@
 package ui;
 
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.Month;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Callback;
 import model.Person;
 
 public class PrincipalController {
@@ -36,7 +54,13 @@ public class PrincipalController {
 	 * window = 7; if UpdateWindw is open.<br>
 	 */
 	private int window = 0;
+
+	/**
+	 * this variable have the code of person selected on the table.<br>
+	 */
+	private String codeTemp = "";
 	
+	private Person p = null;
 	//=============================================================
 	//ADD WINDOW
 	@FXML
@@ -67,6 +91,9 @@ public class PrincipalController {
 	@FXML
     private BorderPane deletePanel;
 
+	@FXML
+    private Label code;
+	
     @FXML
     private Label name;
 
@@ -138,18 +165,111 @@ public class PrincipalController {
     @FXML
     private TableColumn<Person, Void> btCol;
     //===============================================================
+    //READ WINDOW
+    @FXML
+    private Label codeR;
+
+    @FXML
+    private Label nameR;
+
+    @FXML
+    private Label lastaNameR;
+
+    @FXML
+    private Label genderR;
+
+    @FXML
+    private Label birthdayDateR;
+
+    @FXML
+    private Label heightR;
+
+    @FXML
+    private Label nationR;
+
+    @FXML
+    private ImageView photo;
+    //===============================================================
 	public PrincipalController() {
 		
 	}
-	/*
+	
+	/**
+	 *this method initialize the tableView at search windows.<br>
+	 */
 	public void initTable() {
-		/ObservableList<Person>temp = FXCollections.observableArrayList(new Person("Julian", "1234", "Rivera", 'M'), new Person("David", "Fiat"), new Person("Johan"));
+		ObservableList<Person>temp = FXCollections.observableArrayList(
+				new Person("1234", "Julian", "Rivera", 'M', "2001/11/29", 171, "Colombiano"),
+				new Person("4567", "David", "Jhun", 'M', "2000/02/15", 172, "Coreano"),
+				new Person("1596", "David", "Fiat", 'M', "1999/08/12", 175, "Español"),
+				new Person("1468", "Rosa", "Melano", 'F', "2006/06/06", 150, "Sueca"));
 		
-		column1.setCellValueFactory(new PropertyValueFactory<Person, String>("name"));
-		column2.setCellValueFactory(new PropertyValueFactory<Person, Button>("bt"));
-		
+		codigoCol.setCellValueFactory(new PropertyValueFactory<Person, String>("code"));
+		nombreCol.setCellValueFactory(new PropertyValueFactory<Person, String>("name"));
+		apellidoCol.setCellValueFactory(new PropertyValueFactory<Person, String>("lastName"));
+		sexoCol.setCellValueFactory(new PropertyValueFactory<Person, String>("sex"));
 		table.setItems(temp);
-	}*/
+		
+		addButtonToTable();
+	}
+	
+	/*
+	 *this method put a button in each row of the tableView. <br>
+	 */
+	private void addButtonToTable() {
+        Callback<TableColumn<Person, Void>, TableCell<Person, Void>> cellFactory = new Callback<TableColumn<Person, Void>, TableCell<Person, Void>>() {
+            @Override
+            public TableCell<Person, Void> call(final TableColumn<Person, Void> param) {
+                final TableCell<Person, Void> cell = new TableCell<Person, Void>() {
+
+                    private final Button btn = new Button("Editar");
+
+                    {
+                        btn.setOnAction((ActionEvent event) -> {
+                        	p = getTableView().getItems().get(getIndex());
+                        	update(event);
+                        	System.out.println(p.getName());
+                        });
+                    }
+
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(btn);
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+
+        btCol.setCellFactory(cellFactory);
+
+    }
+	public void getUpdate() {
+		if (p != null) {
+			codeTemp = p.getCode();
+			naFieldUpdate.setText(p.getName());
+			laFiedlUpdate.setText(p.getLastName());
+			if (p.getSex() == Person.MASCULINE) {
+				maleUpdate.setSelected(true);
+			}else {
+				femaleUpdate.setSelected(true);
+			}
+			String aux = p.getDate();
+			String[] temp = aux.split("/");
+			int day = Integer.parseInt(temp[2]);
+			int month = Integer.parseInt(temp[1]);
+			int year = Integer.parseInt(temp[0]);
+			
+			birthdayUpdate.setValue(LocalDate.of(year, Month.of(month), day));
+			heFieldUpdate.setText(String.valueOf(p.getHeight()));
+			nationFUpdate.setText(p.getNationality());
+		}
+	}
 	
 	/**
 	 * This method open the ADD interface.<br>
@@ -163,26 +283,9 @@ public class PrincipalController {
     		
     		try {
 				Parent root = fxml.load();
+				mainPanel.getChildren().clear();
+				mainPanel.setCenter(root);
 				window = 1;
-				
-				switch (window) {
-				case 2:
-					deletePanel.getChildren().clear();
-					deletePanel.setCenter(root);
-					break;
-				case 3:
-					searchPanel.getChildren().clear();
-					searchPanel.setCenter(root);
-					break;
-				case 4:
-					updatePanel.getChildren().clear();
-					updatePanel.setCenter(root);
-					break;
-				default:
-					mainPanel.getChildren().clear();
-					mainPanel.setCenter(root);
-					break;
-				}
 				
 			} catch (IOException io) {
 				io.printStackTrace();
@@ -194,6 +297,15 @@ public class PrincipalController {
 			alert.setContentText(null);
 			alert.showAndWait();
 		}
+    }
+    
+    /**
+     * this method saves the entered information of a new user. <br>
+     * @param event
+     */
+    @FXML
+    void save(ActionEvent event) {
+    	System.err.println("Funcionalidad proximamente");
     }
 
     /**
@@ -207,26 +319,18 @@ public class PrincipalController {
     		fxml.setController(this);
     		
     		try {
-				Parent root = fxml.load();
-				window = 2;
-				
-				switch (window) {
-				case 1:
-					addPanel.getChildren().clear();
-					addPanel.setCenter(root);
-					break;
-				case 3:
-					searchPanel.getChildren().clear();
-					searchPanel.setCenter(root);
-					break;
-				case 4:
-					updatePanel.getChildren().clear();
-					updatePanel.setCenter(root);
-					break;
-				default:
+    			if (p == null) {
+					Alert alert = new Alert(AlertType.WARNING);
+	    			alert.setTitle("WARNING");
+	    			alert.setHeaderText("Debe primero seleccionar una persona de la tabla");
+	    			alert.setContentText(null);
+	    			alert.showAndWait();
+				}else {
+					Parent root = fxml.load();
+					window = 2;
 					mainPanel.getChildren().clear();
 					mainPanel.setCenter(root);
-					break;
+					
 				}
 				
 			} catch (IOException io) {
@@ -242,6 +346,33 @@ public class PrincipalController {
     }
 
     /**
+     * this method remove a object of system. <br>
+     */
+    public void remove() {
+    	if (p !=null) {
+    		codeTemp = p.getCode();
+			code.setText(codeTemp);
+			name.setText(p.getName());
+			lastaName.setText(p.getLastName());
+			if (p.getSex() == Person.MASCULINE) {
+				gender.setText("Masculino");
+			}else {
+				gender.setText("Femenino");
+			}
+			birthdayDate.setText(p.getDate());
+			height.setText(String.valueOf(p.getHeight()));
+			nation.setText(p.getNationality());
+			
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setHeaderText(null);
+			alert.setContentText("Se ha eliminado la persona del sistema");
+			alert.showAndWait();
+			
+			p = null;
+    	}
+    }
+    
+    /**
 	 * This method open the SEARCH interface by name.<br>
 	 * @param event
 	 */
@@ -253,27 +384,10 @@ public class PrincipalController {
     		
     		try {
     			Parent root = fxml.load();
-    			//initTable();
+    			mainPanel.getChildren().clear();
+				mainPanel.setCenter(root);
 				window = 3;
-				
-				switch (window) {
-				case 1:
-					addPanel.getChildren().clear();
-					addPanel.setCenter(root);
-					break;
-				case 2:
-					deletePanel.getChildren().clear();
-					deletePanel.setCenter(root);
-					break;
-				case 4:
-					updatePanel.getChildren().clear();
-					updatePanel.setCenter(root);
-					break;
-				default:
-					mainPanel.getChildren().clear();
-					mainPanel.setCenter(root);
-					break;
-				}
+    			initTable();
 				
 			} catch (IOException io) {
 				io.printStackTrace();
@@ -299,27 +413,10 @@ public class PrincipalController {
     		
     		try {
     			Parent root = fxml.load();
-				window = 3;
-				
-				
-				switch (window) {
-				case 1:
-					addPanel.getChildren().clear();
-					addPanel.setCenter(root);
-					break;
-				case 2:
-					deletePanel.getChildren().clear();
-					deletePanel.setCenter(root);
-					break;
-				case 4:
-					updatePanel.getChildren().clear();
-					updatePanel.setCenter(root);
-					break;
-				default:
-					mainPanel.getChildren().clear();
-					mainPanel.setCenter(root);
-					break;
-				}
+    			mainPanel.getChildren().clear();
+				mainPanel.setCenter(root);
+				window = 4;
+				initTable();
 				
 			} catch (IOException io) {
 				io.printStackTrace();
@@ -345,26 +442,10 @@ public class PrincipalController {
     		
     		try {
     			Parent root = fxml.load();
-				window = 3;
-				
-				switch (window) {
-				case 1:
-					addPanel.getChildren().clear();
-					addPanel.setCenter(root);
-					break;
-				case 2:
-					deletePanel.getChildren().clear();
-					deletePanel.setCenter(root);
-					break;
-				case 4:
-					updatePanel.getChildren().clear();
-					updatePanel.setCenter(root);
-					break;
-				default:
-					mainPanel.getChildren().clear();
-					mainPanel.setCenter(root);
-					break;
-				}
+    			mainPanel.getChildren().clear();
+				mainPanel.setCenter(root);
+				window = 5;
+				initTable();
 				
 			} catch (IOException io) {
 				io.printStackTrace();
@@ -390,26 +471,10 @@ public class PrincipalController {
     		
     		try {
     			Parent root = fxml.load();
-				window = 3;
-				
-				switch (window) {
-				case 1:
-					addPanel.getChildren().clear();
-					addPanel.setCenter(root);
-					break;
-				case 2:
-					deletePanel.getChildren().clear();
-					deletePanel.setCenter(root);
-					break;
-				case 4:
-					updatePanel.getChildren().clear();
-					updatePanel.setCenter(root);
-					break;
-				default:
-					mainPanel.getChildren().clear();
-					mainPanel.setCenter(root);
-					break;
-				}
+    			mainPanel.getChildren().clear();
+				mainPanel.setCenter(root);
+				window = 6;
+				initTable();
 				
 			} catch (IOException io) {
 				io.printStackTrace();
@@ -424,39 +489,31 @@ public class PrincipalController {
     }
     /**
 	 * This method open the UPDATE interface.<br>
-	 * @param event
 	 */
     @FXML
 	void update(ActionEvent event) {
     	if (window != 7) {
-    		FXMLLoader fxml = new FXMLLoader(getClass().getResource("UpdateWindow.fxml"));
-    		fxml.setController(this);
     		
-    		try {
-				Parent root = fxml.load();
-				window = 4;
-				
-				switch (window) {
-				case 1:
-					addPanel.getChildren().clear();
-					addPanel.setCenter(root);
-					break;
-				case 2:
-					deletePanel.getChildren().clear();
-					deletePanel.setCenter(root);
-					break;
-				case 3:
-					searchPanel.getChildren().clear();
-					searchPanel.setCenter(root);
-					break;
-				default:
-					mainPanel.getChildren().clear();
-					mainPanel.setCenter(root);
-					break;
-				}
-			} catch (IOException io) {
-				io.printStackTrace();
-			}
+    			FXMLLoader fxml = new FXMLLoader(getClass().getResource("UpdateWindow.fxml"));
+        		fxml.setController(this);
+        		
+        		try {
+        			if (p == null) {
+    				Alert alert = new Alert(AlertType.WARNING);
+    	    			alert.setTitle("WARNING");
+    	    			alert.setHeaderText("Debe primero seleccionar una persona de la tabla");
+    	    			alert.setContentText(null);
+    	    			alert.showAndWait();
+    				}else {
+    					Parent root = fxml.load();
+    					mainPanel.getChildren().clear();
+    					mainPanel.setCenter(root);
+    					window = 7;
+						getUpdate();
+					}
+    			} catch (IOException io) {
+    				io.printStackTrace();
+    			}
 		}else {
 			Alert alert = new Alert(AlertType.WARNING);
 			alert.setTitle("WARNING");
@@ -464,5 +521,104 @@ public class PrincipalController {
 			alert.setContentText(null);
 			alert.showAndWait();
 		}
+    }
+    
+    /**
+     * this method update a object in the system.<br>
+     * @param event
+     */
+    @FXML
+    void actualize(ActionEvent event) {
+    	String name = naFieldUpdate.getText();
+    	String last = laFiedlUpdate.getText();
+    	
+    	char sex = ' ';
+    	if (maleUpdate.isSelected()) {
+			sex = Person.MASCULINE;
+		}if (femaleUpdate.isSelected()) {
+			sex = Person.FEMENINE;
+		}
+		LocalDate temp = birthdayUpdate.getValue();
+		String date = String.valueOf(temp);
+		
+		int height = Integer.parseInt(heFieldUpdate.getText());
+		String nation = nationFUpdate.getText();
+		getNewPerson(new Person(null, name, last, sex, date, height, nation));
+		
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setContentText("Se ha actualizado la persona en el sistema");
+		alert.setHeaderText(null);
+		alert.showAndWait();
+		
+		p = null;
+    }
+    
+    public Person getNewPerson(Person p) {
+    	p.setCode(codeTemp);
+    	return p;
+    }
+    
+    void showing() {
+    	FXMLLoader fxml = new FXMLLoader(getClass().getResource("ReadWindow.fxml"));
+		fxml.setController(this);
+		 
+		try {
+				Parent root = fxml.load();
+				Stage stage = new Stage();
+				Scene scene = new Scene(root);
+		        stage.initModality(Modality.APPLICATION_MODAL);
+		        stage.initStyle(StageStyle.DECORATED);
+		        stage.setTitle("VISUALIZACION");
+		        stage.setResizable(true);
+		        stage.setScene(scene);
+		        stage.show();
+		        
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+    }
+    
+    @FXML
+    void show(ActionEvent event) throws IOException {
+    	Person temp = table.getSelectionModel().getSelectedItem();
+    	if (temp !=null) {
+			showing();
+			nameR.setText(temp.getName());
+			lastaNameR.setText(temp.getLastName());
+			codeR.setText(temp.getCode());
+			
+			if (temp.getSex() == Person.MASCULINE) {
+				genderR.setText("Masculino");
+			}else {
+				genderR.setText("Femenino");
+			}
+			birthdayDateR.setText(temp.getDate());
+			heightR.setText(String.valueOf(temp.getHeight()));
+			nationR.setText(temp.getNationality());
+			
+			loadImage();
+		}else {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("WARNING");
+			alert.setHeaderText("Primero debes buscar una persona y seleccionarla");
+			alert.setContentText(null);
+			alert.showAndWait();
+		}
+    }
+    
+    public void loadImage() throws IOException {
+    	String page = "https://thispersondoesnotexist.com/image";
+        URL url = new URL(page);
+        /*final HttpURLConnection image = (HttpURLConnection) url
+                .openConnection();
+        image.setRequestProperty(
+                "User-Agent",
+                "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+        BufferedImage bufferedImage = ImageIO.read(image.getInputStream());
+        Image picture = SwingFXUtils.toFXImage(bufferedImage, null);*/
+      
+        //ImageIcon intento = new ImageIcon(url);
+        Image picture = new Image(page);
+        photo.setImage(picture);
     }
 }
